e8fb89fad899f417c137003baa0549518006832d
usuless <61857692+usuless@users.noreply.github.com>
2023-12-29 22:58:37 +0100

    clock and event listener

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("babajaga");
+// zegar
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const formattedTime = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+
+    clockElement.textContent = formattedTime;
+  }
+
+  function padZero(number) {
+    return number < 10 ? '0' + number : number;
+  }
+
+  setInterval(updateClock, 1000);
+
+  updateClock();
+
+  // dodawanie zadań
+
+  $(".btn").on("click", function getInput() {
+    let inputValue = $("#taskInput").val();
+    console.log(inputValue)
+  })
