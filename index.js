96a1c479b957969a7fb01606f0eadc89a88ea07c
usuless <61857692+usuless@users.noreply.github.com>
2023-12-30 14:19:19 +0100

    działa, a nie powinno

--- index.js
+++ index.js
@@ -21,16 +21,41 @@ function updateClock() {
 
   // dodawanie zadań
 
-  let input = $("#taskInput").value;
+  let input = $("#taskInput").val();
+  let taskNumber = 0;
 
   $(".btn").on("click", function getInput() {
     let inputValue = $("#taskInput").val();
     if (inputValue == '') {
         return;
     } else {
+    taskNumber++;
+    let taskNumberClass = "taskNo" + taskNumber;
+    let container = $("<div>");
     let newDiv = $("<div>").text(inputValue);
-    $(".list-of-task").append(newDiv);
+    var button = $('<button>', {
+      text: 'Click Me',
+      click: function deleteTask() {
+
+    // usuwanie zadań
+
+        $("#" + taskNumberClass).remove();
+        taskNumber--;
+      }
+    });
+
+    // dodawanie elemntów na stronę
+    
+    $(".list-of-task").append(container);
+    $(container).append(newDiv);
+    $(container).append(button);
+    $(container).addClass("taskContainer");
+    $(container).attr('id', taskNumberClass);
     $(newDiv).addClass("task");
+    $(newDiv).attr('id', taskNumberClass);
+    $(button).addClass("taskButton");
+    $(button).attr('id', taskNumberClass);
     $("#taskInput").val('');
     }
   })
+
